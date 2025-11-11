655bb4bb050a3764bd7b510a4fb8dfa93b8b0f2f
Ranjith282006 <ranjithr.cs25@bmsce.ac.in>
2025-11-11 08:59:01 +0530

    Update dll.c

--- dll.c
+++ dll.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
-// Define the structure for a doubly linked list node
+
 typedef struct Node {
 int data;
 struct Node* prev;
 struct Node* next;
 } Node;
-// Function to create a new node with given data
 Node* createNode(int value) {
 Node* newNode = (Node*)malloc(sizeof(Node));
 newNode->data = value;
@@ -15,7 +14,6 @@ newNode->prev = NULL;
 newNode->next = NULL;
 return newNode;
 }
-// Function to display the doubly linked list with pointer addresses
 void displayList(Node* head) {
 Node* temp = head;
 printf("\nDoubly Linked List Structure:\n");
@@ -33,13 +31,11 @@ temp = temp->next;
 printf("-------------------------------------------------------------\n");
 }
 int main() {
-// Step 1: Create 5 independent nodes
 Node* N1 = createNode(1);
 Node* N2 = createNode(2);
 Node* N3 = createNode(3);
 Node* N4 = createNode(4);
 Node* N5 = createNode(5);
-// Step 2: Link nodes to form the doubly linked list
 N1->next = N2;
 N2->prev = N1;
 N2->next = N3;
@@ -48,9 +44,7 @@ N3->next = N4;
 N4->prev = N3;
 N4->next = N5;
 N5->prev = N4;
-// Step 3: Display the list with pointer addresses
 displayList(N1);
-// Cleanup: Free allocated memory
 free(N1);
 
 free(N2);
@@ -59,3 +53,4 @@ free(N4);
 free(N5);
 return 0;
 }
+
